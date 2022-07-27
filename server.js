8a32a04d91e89bfd601b54ca14f8f3ab3c408205
Nolan Frazelle <frazellen@gmail.com>
2022-07-27 10:19:43 -0700

    commented on 20 lines, break

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const app = express()
-const MongoClient = require('mongodb').MongoClient
-const PORT = 2121
-require('dotenv').config()
+const express = require('express') // Setting up express framework to allow for easier server construction
+const app = express() // Setting a variable equal to the express framework so that we can easiy tell it what to do and improve readability. 
+const MongoClient = require('mongodb').MongoClient // Setting up the database and telling express what to use and telling MongoDB some stuff
+const PORT = 2121 // Local port for viewing content
+require('dotenv').config() // Setting up the enviorment variables from a .env file
 
 
-let db,
-    dbConnectionStr = process.env.DB_STRING,
-    dbName = 'todo'
+let db, // Defining variables 
+    dbConnectionStr = process.env.DB_STRING, // Telling the server where to find the connection string for the database, inside a variable from a .env file
+    dbName = 'todo' // Defining the name of the database to use throughout the document
 
-MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
-    .then(client => {
-        console.log(`Connected to ${dbName} Database`)
-        db = client.db(dbName)
-    })
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }) // Initiate connection to database
+    .then(client => { // Treated as a promise, once returned, it runs the following code
+        console.log(`Connected to ${dbName} Database`) // Logging the fact that we are connected, to make sure that the connection was successful
+        db = client.db(dbName) // Telling the database what its name that are asking of it to be
+    }) // Finialize code for connection to DB
     
-app.set('view engine', 'ejs')
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.set('view engine', 'ejs') // Telling express to use ejs as a templating engine
+app.use(express.static('public')) // This helps to serve static files such as images, CSS, JS using a middleware that is built in to express
+app.use(express.urlencoded({ extended: true })) // Another built in middleware that is built into express that acts as body-parser
+app.use(express.json()) // Another built in middleware that is built into express that parses incoming JSON requests and puts the parsed data in the req.body
 
 
 app.get('/',async (request, response)=>{
