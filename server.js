e90ca4b652a94c7538dea6d4f8f2f33c502c1d11
Nolan Frazelle <frazellen@gmail.com>
2022-07-27 11:54:37 -0700

    commented every line, HW complete

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ app.use(express.urlencoded({ extended: true })) // Another built in middleware t
 app.use(express.json()) // Another built in middleware that is built into express that parses incoming JSON requests and puts the parsed data in the req.body
 
 
-app.get('/',async (request, response)=>{
-    const todoItems = await db.collection('todos').find().toArray()
-    const itemsLeft = await db.collection('todos').countDocuments({completed: false})
-    response.render('index.ejs', { items: todoItems, left: itemsLeft })
+app.get('/',async (request, response)=>{ // Telling express what to do when express gets a ping from '/', usually the homepage. 
+    const todoItems = await db.collection('todos').find().toArray() // This sets a variable equal to the data returned in an array
+    const itemsLeft = await db.collection('todos').countDocuments({completed: false}) // This sets another variable equal to the number of items that are present in that collection
+    response.render('index.ejs', { items: todoItems, left: itemsLeft }) // This line tells express when it hears that ping of this page then it will respond with a render with the passed in file and the passed in data that will accompany the response.
     // db.collection('todos').find().toArray()
     // .then(data => {
     //     db.collection('todos').countDocuments({completed: false})
@@ -35,59 +35,60 @@ app.get('/',async (request, response)=>{
     // .catch(error => console.error(error))
 })
 
-app.post('/addTodo', (request, response) => {
-    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false})
-    .then(result => {
-        console.log('Todo Added')
-        response.redirect('/')
+app.post('/addTodo', (request, response) => { // Telling express to post something based on the passed in location. 
+    db.collection('todos').insertOne({thing: request.body.todoItem, completed: false}) // This tells express and the database to create a new item inside of the collection with the passed in data in the object
+    .then(result => { // This line tells express once the promise has been fulfilled to run this code below
+        console.log('Todo Added') // Logging the fact that it was added for my sake and for clarity's sake
+        response.redirect('/') // Once it has added the item to the db it has to refresh andd this helps us do that by telling express to render the '/' route from above
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error)) // If the addTodo has an error, this will tell us the error in the log
 })
 
-app.put('/markComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
-        $set: {
-            completed: true
+app.put('/markComplete', (request, response) => { // Telling express to update something based on the passed in location
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{ // This tells our db to update a certain thing within the item and that thing is the passed in object.
+        $set: { // This operator allows us to replace the value of a field with the specefied value
+            completed: true // This is the specefied value that is being changed
+          } 
+    },{
+        sort: {_id: -1}, // This tell our db to sort the items based on the passed in function
+        upsert: false // This tells our db to sort the ones of false 
+    })
+    .then(result => { // Once the promise has been fulfilled it runs this then statement
+        console.log('Marked Complete') // This logs the fact that it was marked for our sake
+        response.json('Marked Complete') // This tells express what to response to
+    })
+    .catch(error => console.error(error)) // If their is an error this will catch it and display it on the log
+
+})
+
+app.put('/markUnComplete', (request, response) => { // This tells express to update something based on a specefied location
+    db.collection('todos').updateOne({thing: request.body.itemFromJS},{ // This tells express to search within a collection within our db and updateOne. This thing we are updating is the passed in object 
+        $set: { // This operator allows us to replace the value of a field with the passed in value
+            completed: false // The set operator looks to change the 'completed' field with the value of 'false'
           }
     },{
-        sort: {_id: -1},
-        upsert: false
+        sort: {_id: -1}, // This then sorts the data by the passed in object 
+        upsert: false // This tells our db to sort the ones of false
     })
-    .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
+    .then(result => { //Once the promise has been fulfilled it runs this then statement
+        console.log('Marked Complete') // This logs the fact that it was unmarked accordingly
+        response.json('Marked Complete') // This tells express what to response to
     })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error)) // If their is an error this will catch it and display it on the log
 
 })
 
-app.put('/markUnComplete', (request, response) => {
-    db.collection('todos').updateOne({thing: request.body.itemFromJS},{
-        $set: {
-            completed: false
-          }
-    },{
-        sort: {_id: -1},
-        upsert: false
+app.delete('/deleteItem', (request, response) => { // This tells express to delete something based on listening to a specefied location
+    db.collection('todos').deleteOne({thing: request.body.itemFromJS}) // This tells us and our db where to look to delete something and what to delete
+    .then(result => { // After the promise has been fulfilled it runs this then statement
+        console.log('Todo Deleted') // This logs the fact that it was deleted for our sake and clarity
+        response.json('Todo Deleted') // This tells express what to respond with
     })
-    .then(result => {
-        console.log('Marked Complete')
-        response.json('Marked Complete')
-    })
-    .catch(error => console.error(error))
+    .catch(error => console.error(error)) // If their is an error this will catch it and display it on the log
 
 })
 
-app.delete('/deleteItem', (request, response) => {
-    db.collection('todos').deleteOne({thing: request.body.itemFromJS})
-    .then(result => {
-        console.log('Todo Deleted')
-        response.json('Todo Deleted')
-    })
-    .catch(error => console.error(error))
+app.listen(process.env.PORT || PORT, ()=>{ // This tells express where to set up its server, either in the .env file where a variable holds the connection string or through a definded port that is local
+    console.log(`Server running on port ${PORT}`) // This tells us our server is up and running
+}) // fin 
 
-})
-
-app.listen(process.env.PORT || PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
-})
